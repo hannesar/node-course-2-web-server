886dda786292038569747e2b90ff5c76f074ff89
unknown <hannesaronsson@gmail.com>
2018-10-10 18:13:55 -0700

    Fixing an error in code

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/about', (req, res) => {
   });
 });
 
-app.get('/projects', req, res) => {
+app.get('/projects', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'Projects'
   });
